ba48c2ca61fb278ee93f8aa2e236c66b9913e0c4
Javier Aguilar <coyote176@msn.com>
2020-08-08 21:00:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -52,8 +52,8 @@ def monopoly_with_fourth_tier
 	    pieces: 4,
 	    names: {reading_railroad:{["mortgage_valu": "$100"]},
 	            pennsylvania_railroad: {["mortgage_value": "$200"]},
-	            b_and_o_railroad: {[]},
-	            shortline: {}
+	            b_and_o_railroad: {["mortgage_value": "$400"]},
+	            shortline: {[]}
 	    },
 	    rent_in_dollars: {one_piece_owned: 25,
 	                      two_pieces_owned: 50,
